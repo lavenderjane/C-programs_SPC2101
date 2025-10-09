9bce02423eb75f387ba0f927210828228f73330e
lavenderjane <lavenderjane19@gmail.com>
2025-10-09 11:40:54 +0300

    Add files via upload

---
+++ password.c
@@ -0,0 +1,21 @@
+/*
+NAME- JANE NJERI MUTHONI 
+REG NO- CT101/G/26472/25
+COURSE-COMPUTER SCIENCE,group A
+DISCRIPTION-Passsword verification
+ 
+*/
+#include<stdio.h>
+int main(){
+	
+	int password=1234;
+	
+	do
+	{
+		printf("enter your password:");
+		scanf("%d",& password);
+	}
+	while(password!=1234);
+	printf("access granted",password=1234);
+	return 0;
+}
